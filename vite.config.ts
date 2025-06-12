b004d2b38e4dd51db1929a280d2a626450b60a19
Scott Mains <scottmains@hotmail.co.uk>
2025-06-12 12:40:22 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,23 @@
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite'
-import fs from 'fs';
-import process from 'process';
+import tailwindcss from '@tailwindcss/vite';
+import * as fs from 'fs';
 
 // Determine if we're building for production
 const isProduction = process.env.NODE_ENV === 'production';
 
-// Only set up HTTPS for development
-const serverConfig = isProduction 
-  ? { port: 3000 } 
-  : {
-      port: 3000,
-      https: {
-        key: fs.readFileSync('localhost+1-key.pem'),
-        cert: fs.readFileSync('localhost+1.pem'),
-      },
-    };
+let serverConfig: any = { port: 3000 };
+if (!isProduction) {
+  serverConfig = {
+    port: 3000,
+    https: {
+      key: fs.readFileSync('localhost+1-key.pem'),
+      cert: fs.readFileSync('localhost+1.pem'),
+    },
+  };
+}
+
 
 export default defineConfig({
   base: '/', // Changed from './' to '/'
